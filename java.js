d919418691823c76ddf9b902ea84c21cde7d7a0e
ryan-ryu8 <64135999+ryan-ryu8@users.noreply.github.com>
2020-05-17 19:33:06 +0300

    Update java.js

--- java.js
+++ java.js
@@ -1,5 +1,5 @@
 var CC, YY, MM, DD, d, dayValue;
-var dayNmaes = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+ dayNmaes = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
